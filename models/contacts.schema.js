2f9eb0cf98b8da60b86efc557686df5a78dcadf4
jorgelnc <jorgenavacolina@gmail.com>
2024-01-09 13:20:25 -0500

    Updated files task 3

---
+++ models/contacts.schema.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+
+const contactSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+        },
+    email: {
+        type: String,
+        },
+    phone: {
+        type: String,
+        },
+    favorite: {
+        type: Boolean,
+        default: false,
+        },
+    });
+
+module.exports = model('contacts', contactSchema);
